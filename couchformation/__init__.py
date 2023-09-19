48ae541531f03f86baa953c61dcaaded83f2faf3
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-09-19 11:42:16 -0500

    'Version 4.0.0a7'

--- couchformation/__init__.py
+++ couchformation/__init__.py
@@ -2,5 +2,5 @@ import os
 from pkg_resources import parse_version
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-__version__ = "4.0.0a6"
+__version__ = "4.0.0a7"
 VERSION = parse_version(__version__)
