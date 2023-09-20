faa7e1477f75749b2b8440ae48536a37ff7316e8
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-09-19 19:42:19 -0500

    'Version 4.0.0a8'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Couch Formation 4.0.0a7
+# Couch Formation 4.0.0a8
 
 ![Logo](https://raw.githubusercontent.com/mminichino/couch-formation-core/main/doc/couch-formation-1.png)
 
--- couchformation/__init__.py
+++ couchformation/__init__.py
@@ -2,5 +2,5 @@ import os
 from pkg_resources import parse_version
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-__version__ = "4.0.0a7"
+__version__ = "4.0.0a8"
 VERSION = parse_version(__version__)
