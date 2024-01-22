6c0a8b7a5b7ad62142aabd29199cc7878906e61e
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-01-22 15:59:26 -0600

    Version 4.0.0a26

--- couchformation/util.py
+++ couchformation/util.py
@@ -8,8 +8,6 @@ import collections
 from typing import Union
 from uuid import UUID
 from shutil import copyfile
-from pwd import getpwnam
-from grp import getgrnam
 from multiprocessing import Lock
 from pyhostprep.command import RunShellCommand
 from couchformation.exception import FatalError
@@ -41,31 +39,15 @@ class FileManager(object):
     def __init__(self):
         pass
 
-    def make_dir(self, name: str, owner: str = None, group: str = None, mode: int = 0o775):
-        owner_id = getpwnam(owner).pw_uid if owner else None
-        group_id = getgrnam(group).gr_gid if group else None
+    def make_dir(self, name: str):
         if not os.path.exists(name):
             path_dir = os.path.dirname(name)
             if not os.path.exists(path_dir):
                 self.make_dir(path_dir)
             try:
-                uid = os.stat(path_dir).st_uid if not owner_id else owner_id
-                gid = os.stat(path_dir).st_gid if not group_id else group_id
-                if not os.path.exists(name):
-                    os.mkdir(name)
-                    os.chown(name, uid, gid)
-                    os.chmod(name, mode)
+                os.mkdir(name)
             except OSError:
                 raise
-
-    def set_perms(self, name: str, owner: str, group: str, mode: int = 0o775):
-        if os.path.exists(name):
-            uid = getpwnam(owner).pw_uid
-            gid = getgrnam(group).gr_gid
-            os.chown(name, uid, gid)
-            os.chmod(name, mode)
-        else:
-            self.make_dir(name, owner, group, mode)
 
     def dir_populate(self, path: str, command: Union[str, None] = None):
         self.make_dir(path)
