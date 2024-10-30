bac20c6dffbe63a15826816b8324c3165d1c7967
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2024-10-29 19:24:55 -0500

    Version 4.0.0

--- couchformation/__init__.py
+++ couchformation/__init__.py
@@ -1,9 +1,7 @@
 import os
-from pkg_resources import parse_version
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 __version__ = "4.0.0"
-VERSION = parse_version(__version__)
 
 
 def get_data_dir():
